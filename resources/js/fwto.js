86ac1875d385b8bc7c2201b71a2a0faaa5267202
sairim <spiri@protonmail.com>
2019-05-09 11:43:18 +0300

    add boilerplate for fwto page

---
+++ resources/js/fwto.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  // execute common tasks
+  SharedJS.init();
+
+  // half circle the header text
+  new CircleType(document.getElementById('header-text'))
+  .dir(-1)
+  .radius(384);
+
+});
