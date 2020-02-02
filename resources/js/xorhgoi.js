0f51d1af864846de3c4fc815aad30fc904cc84dd
methias <tmn5@pm.me>
2020-02-02 08:44:23 +0200

    add xorhgoi page + resource

---
+++ resources/js/xorhgoi.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+  // execute common tasks
+  SharedJS.init();
+
+  // half circle the header text
+  new CircleType(document.getElementById('header-text'))
+  .dir(-1)
+  .radius(384);
+
+  // initialize lightbox for photo gallery
+  $(document).on("click", '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+  });
+});
