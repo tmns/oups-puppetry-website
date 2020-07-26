311c4b30dd7a8d15706fa9f399a8413aa40c3a08
methias <tmn5@pm.me>
2020-07-26 20:29:54 +0300

    Remove some superflous comments from index.js

--- resources/js/index.js
+++ resources/js/index.js
@@ -20,21 +20,4 @@ $(document).ready(function() {
 
   // initialize our animated sections
   AnimateIndexSections.init();
-
-  // // initialize scrollspy
-  // $("body").scrollspy({ target: "#main-nav" });
-
-  // // smooth scroll
-  // $("#main-nav a").on("click", function(event) {
-  //   if (this.hash !== "") {
-  //     event.preventDefault();
-  //     const hash = this.hash;
-  //     $("html, body").animate(
-  //       {
-  //         scrollTop: $(hash).offset().top - 110
-  //       },
-  //       900
-  //     );
-  //   }
-  // });
 });
